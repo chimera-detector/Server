1b21e40597f469abe8b36b248c30411f946351eb
Han_Song <shak5643@gmail.com>
2019-05-12 22:16:12 +0900

    tensorflow adjustment (using default graph whenever it predict)

--- src/detect.py
+++ src/detect.py
@@ -1,10 +1,14 @@
+# usr/bin/python27
+
 from models.convnets import ConvolutionalNet
 from keras.models import load_model
 from keras.preprocessing import sequence
+# from keras.backend import clear_session
 from preprocessors.preprocess_text import clean
 import sys
-import string 
+import string
 import re
+import tensorflow as tf
 
 MATCH_MULTIPLE_SPACES = re.compile("\ {2,}")
 SEQUENCE_LENGTH = 20
@@ -23,18 +27,22 @@ def words_to_indices(inverse_vocabulary, words):
 
 
 
-
 class Predictor (object):
     def __init__(self, model_path):
         model = ConvolutionalNet(vocabulary_size=len(vocabulary), embedding_dimension=EMBEDDING_DIMENSION, input_length=SEQUENCE_LENGTH)
         model.load_weights(model_path)
+        self.graph = tf.get_default_graph()
         self.model = model
-    
+
     def predict (self, headline):
-        headline = headline.encode("ascii", "ignore")
+        # headline = headline.encode("ascii", "ignore")
         inputs = sequence.pad_sequences([words_to_indices(inverse_vocabulary, clean(headline).lower().split())], maxlen=SEQUENCE_LENGTH)
-        clickbaitiness = self.model.predict(inputs)[0, 0]
+        with self.graph.as_default():
+            clickbaitiness = self.model.predict(inputs)[0, 0]
         return clickbaitiness
+
+
+
 predictor = Predictor("models/detector.h5")
 if __name__ == "__main__":
     print ("headline is {0} % clickbaity".format(round(predictor.predict(sys.argv[1]) * 100, 2)))
