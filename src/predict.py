c6e59ba43b7fa3c7911c15bc74473b6470f49b69
Han_Song <shak5643@gmail.com>
2019-05-15 15:45:06 +0900

    Add test data training pipeline

---
+++ src/predict.py
@@ -0,0 +1,183 @@
+# usr/bin/python27
+
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from csv import DictReader
+from csv import DictWriter
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import tensorflow as tf
+
+
+# Initialise global variables
+label_ref = {'agree': 0, 'disagree': 1, 'discuss': 2, 'unrelated': 3}
+label_ref_rev = {0: 'agree', 1: 'disagree', 2: 'discuss', 3: 'unrelated'}
+stop_words = []# TODO: Need to be filled
+
+
+
+# Define data class
+class FNCData:
+
+    """
+        Define class for Fake News Challenge data
+    """
+
+    def __init__(self, file_instances, file_bodies):
+        # Load data
+        self.instances = self.read(file_instances)
+        bodies = self.read(file_bodies)
+        self.heads = {}
+        self.bodies = {}
+
+        # Process instances
+        for instance in self.instances:
+            if instance['Headline'] not in self.heads:
+                head_id = len(self.heads)
+                self.heads[instance['Headline']] = head_id
+            instance['Body ID'] = int(instance['Body ID'])
+
+        # Process bodies
+        for body in bodies:
+            self.bodies[int(body['Body ID'])] = body['articleBody']
+
+
+    def read(self, filename):
+
+        """
+            Read fake news challenge data from CSV file
+
+            Args:
+                filename: str, filename + extension
+
+            Returns:
+                rows: list, of dict per instance
+        """
+
+        # Initialise
+        rows = []
+
+        # Process file
+        with open(filename, "r", encoding='utf-8') as table:
+            r = DictReader(table)
+            for line in r:
+                rows.append(line)
+
+        return rows
+
+
+# Define relevant functions
+def pipeline_train(train, test, lim_unigram):
+
+    """
+        Process train set, create relevant vectorizers
+
+        Args:
+            train: FNCData object, train set
+            test: FNCData object, test set
+            lim_unigram: int, number of most frequent words to consider
+
+        Returns:
+            train_set: list, of numpy arrays
+            train_stances: list, of ints
+            bow_vectorizer: sklearn CountVectorizer
+            tfreq_vectorizer: sklearn TfidTransformer(use_idf=False)
+            tfidf_vectorizer: sklearn TfidfVectorizer()
+    """
+
+    # Initialise
+    heads = []
+    heads_track = {}
+    bodies = []
+    bodies_track = {}
+    body_ids = []
+    id_ref = {}
+    train_set = []
+    train_stances = []
+    cos_track = {}
+    test_heads = []
+    test_bodies = []
+    test_bodies_track = {}
+    test_body_ids = []
+    head_tfidf_track = {}
+    body_tfidf_track = {}
+
+    # Identify unique heads and bodies
+    for instance in train.instances:
+        head = instance['Headline']
+        body_id = instance['Body ID']
+        if head not in heads_track:
+            heads.append(head)
+            heads_track[head] = 1
+        if body_id not in bodies_track:
+            bodies.append(train.bodies[body_id])
+            bodies_track[body_id] = 1
+            bodiy_ids.append(body_id)
+
+    for instance in test.instances:
+        head = instance['Headline']
+        body_id = instance['Body ID']
+        if head not in test_heads_track:
+            test_heads.append(head)
+            test_heads_track[head] = 1
+        if body_id not in test_bodies_track:
+            test_bodies.append(test.bodies[body_id])
+            test_bodies_track[body_id] = 1
+            test_body_ids.append(body_id)
+
+    # Create reference dictionary
+    for i, elem in enumerate(heads + body_ids):
+        id_ref[elem] = i
+        # print(i)
+        # print(elem)
+
+    # Create vectorizers and BOW and TF arrays for train set
+    bow_vectorizer = CountVectorizer(max_features=lim_unigram, stop_words=stop_words)
+    bow = bow_vectorizer.fit_transform(heads + bodies) # Train set only
+
+    tfreq_vectorizer = TfidTransformer(use_idf=False).fit(bow)
+    tfreq = tfreq_vectorizer.transform(bow).toarray() # Train set only
+
+    tfidf_vectorizer = TfidVectorizer(max_features=lim_unigram, stop_words=stop_words).\
+        fit(heads + bodies + test_heads + test_bodies) # Train and test sets
+
+    # Process train set
+    for instance in train.instances:
+        head = instance['Headline']
+        body_id = instance['Body ID']
+        head_tf = tfreq[id_ref[head]].reshape(1, -1)
+        body_tf = tfreq[id_ref[body_id]].reshape(1, -1)
+
+        if head not in head_tfidf_track:
+            head_tfidf = tfidf_vectorizer.transform([head]).toarray()
+            head_tfidf_track[head] = head_tfidf
+        else:
+            head_tfidf = head_tfidf_track[head]
+
+        if body_id not in body_tfidf_track:
+            body_tfidf = tfidf_vectorizer.transform([train.bodies[body_id]]).toarray()
+            body_tfidf_track[body_id] = body_tfidf
+        else:
+            body_tfidf = body_tfidf_track[body_id]
+
+        if (head, body_id) not in cos_track:
+            tfidf_cos = cosine_similarity(head_tfidf, body_tfidf)[0].reshape(1, 1)
+            cos_track[(head, body_id)] = tfidf_cos
+        else:
+            tfidf_cos = cos_track[(head, body_id)]
+
+        feat_vec = np.squeeze(np.c_[had_tf, body_tf, tfidf_cos])
+        train_set.append(feat_vec)
+        train_stances.append(label_ref[instance['Stance']])
+
+    return train_set, train_stances, bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer
