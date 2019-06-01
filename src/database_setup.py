a13e3478e1bd47517b00c4a24fb287f1b14bf0eb
HanSong <shak5643@gmail.com>
2019-06-02 03:58:46 +0900

    add database set up for edition.cnn.com

---
+++ src/database_setup.py
@@ -0,0 +1,48 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class clickbait(Base):
+    __tablename__ = 'clickbait'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(500), nullable=False)
+    clickbaitiness = Column(Integer, nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'title': self.title,
+            'clickbaitiness': self.clickbaitiness
+        }
+
+class stance(Base):
+    __tablename__ = 'stance'
+
+    id     = Column(Integer, primary_key=True)
+    title  = Column(String(500), nullable=False)
+    body   = Column(String(1500), nullable=False)
+    stance = Column(String(250))
+
+     @property
+     def serialize(self):
+         """Return object data in easily serializeable format"""
+         return {
+             'id': self.id,
+             'title': self.title,
+             'stance': self.stance
+         }
+
+
+engine = create_engine('sqlite:///cnn.db')
+
+
+Base.metadata.create_all(engine)
